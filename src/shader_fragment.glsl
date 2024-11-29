38fd9756d4c3c57f1a4c0055667a257d3a569383
giuliastefainski <00289108@ufrgs.br>
2024-11-29 16:52:49 -0300

    use lab4 as template

---
+++ src/shader_fragment.glsl
@@ -0,0 +1,118 @@
+#version 330 core
+
+// Atributos de fragmentos recebidos como entrada ("in") pelo Fragment Shader.
+// Neste exemplo, este atributo foi gerado pelo rasterizador como a
+// interpolação da posição global e a normal de cada vértice, definidas em
+// "shader_vertex.glsl" e "main.cpp".
+in vec4 position_world;
+in vec4 normal;
+
+// Matrizes computadas no código C++ e enviadas para a GPU
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+// Identificador que define qual objeto está sendo desenhado no momento
+#define SPHERE 0
+#define PLANE  2
+uniform int object_id;
+
+// O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
+out vec4 color;
+
+void main()
+{
+    // Obtemos a posição da câmera utilizando a inversa da matriz que define o
+    // sistema de coordenadas da câmera.
+    vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 camera_position = inverse(view) * origin;
+
+    // O fragmento atual é coberto por um ponto que percente à superfície de um
+    // dos objetos virtuais da cena. Este ponto, p, possui uma posição no
+    // sistema de coordenadas global (World coordinates). Esta posição é obtida
+    // através da interpolação, feita pelo rasterizador, da posição de cada
+    // vértice.
+    vec4 p = position_world;
+
+    // Normal do fragmento atual, interpolada pelo rasterizador a partir das
+    // normais de cada vértice.
+    vec4 n = normalize(normal);
+
+    // Vetor que define o sentido da fonte de luz em relação ao ponto atual.
+    vec4 l = normalize(vec4(1.0,1.0,0.5,0.0));
+
+    // Vetor que define o sentido da câmera em relação ao ponto atual.
+    vec4 v = normalize(camera_position - p);
+
+    // Vetor que define o sentido da reflexão especular ideal.
+    vec4 r = -l + 2*n*(dot(n,l)); // vetor de reflexão especular ideal
+
+    // Parâmetros que definem as propriedades espectrais da superfície
+    vec3 Kd; // Refletância difusa
+    vec3 Ks; // Refletância especular
+    vec3 Ka; // Refletância ambiente
+    float q; // Expoente especular para o modelo de iluminação de Phong
+
+    if ( object_id == SPHERE )
+    {
+        // Propriedades espectrais da esfera
+        Kd = vec3(0.8, 0.4, 0.08);
+        Ks = vec3(0.0, 0.0, 0.0);
+        Ka = vec3(0.4, 0.2, 0.04);
+        q = 1.0;
+    }
+    else if ( object_id == PLANE )
+    {
+        // Propriedades espectrais do plano
+        Kd = vec3(0.2, 0.2, 0.2);
+        Ks = vec3(0.3, 0.3, 0.3);
+        Ka = vec3(0.0, 0.0, 0.0);
+        q = 20.0;
+    }
+    else // Objeto desconhecido = preto
+    {
+        Kd = vec3(0.0, 0.0, 0.0);
+        Ks = vec3(0.0, 0.0, 0.0);
+        Ka = vec3(0.0, 0.0, 0.0);
+        q = 1.0;
+    }
+
+    // Espectro da fonte de iluminação
+    vec3 I = vec3(1.0, 1.0, 1.0); // espectro da fonte de luz
+
+    // Espectro da luz ambiente
+    vec3 Ia = vec3(0.2, 0.2, 0.2); // espectro da luz ambiente
+
+    // Termo difuso utilizando a lei dos cossenos de Lambert
+    
+    vec3 lambert_diffuse_term =  Kd * I * dot(n,l); // termo difuso de Lambert: 𝑘𝑑 𝐼 max(0, 𝑛 ⋅ 𝑙), em que n . l = cos(theta)
+
+    // Termo ambiente
+    vec3 ambient_term = Ka * Ia; // termo ambiente
+
+    // Termo especular utilizando o modelo de iluminação de Phong
+    vec3 phong_specular_term  = Ks * I * pow(max(0, dot(r, v)), q); // termo especular de Phong
+
+    // NOTE: Se você quiser fazer o rendering de objetos transparentes, é
+    // necessário:
+    // 1) Habilitar a operação de "blending" de OpenGL logo antes de realizar o
+    //    desenho dos objetos transparentes, com os comandos abaixo no código C++:
+    //      glEnable(GL_BLEND);
+    //      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // 2) Realizar o desenho de todos objetos transparentes *após* ter desenhado
+    //    todos os objetos opacos; e
+    // 3) Realizar o desenho de objetos transparentes ordenados de acordo com
+    //    suas distâncias para a câmera (desenhando primeiro objetos
+    //    transparentes que estão mais longe da câmera).
+    // Alpha default = 1 = 100% opaco = 0% transparente
+    color.a = 1;
+
+    // Cor final do fragmento calculada com uma combinação dos termos difuso,
+    // especular, e ambiente. Veja slide 129 do documento Aula_17_e_18_Modelos_de_Iluminacao.pdf.
+    color.rgb = lambert_diffuse_term + ambient_term + phong_specular_term;
+
+    // Cor final com correção gamma, considerando monitor sRGB.
+    // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+    color.rgb = pow(color.rgb, vec3(1.0,1.0,1.0)/2.2);
+} 
+
