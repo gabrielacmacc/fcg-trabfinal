11ef132692d1110269e5415deca84f88fc8b8f0b
Gabriela Maccagnan <gabrielacmacc@gmail.com>
2025-01-03 11:56:37 -0300

    render cherries

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,35 @@ void MovePacman(glm::vec4 camera_view_unit, glm::vec4 camera_side_view_unit, flo
 // Declaração da classe paredes
 using namespace std;
 
+class Cherry
+{
+public:
+    // Atributos:
+    glm::mat4 modelMatrix;
+    int objectId;
+    int objectType;
+    std::string objectName;
+    glm::vec3 center;
+    Sphere cherry_sphere;
+
+    // Métodos:
+
+    // Construtor
+    Cherry(glm::mat4 modelMatrix, int objectId, int objectType, std::string objectName, glm::vec3 center, float radius)
+        : modelMatrix(modelMatrix), objectId(objectId), objectType(objectType), objectName(objectName)
+    {
+        this->center = center;
+        this->cherry_sphere = Sphere{center, radius};
+    }
+
+    void render()
+    {
+        glUniformMatrix4fv(g_model_uniform, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+        glUniform1i(g_object_id_uniform, objectType);
+        DrawVirtualObject(objectName.c_str());
+    }
+};
+
 class Ghost
 {
 public:
@@ -110,6 +139,7 @@ private:
         return {modelMatrix * minCorner, modelMatrix * maxCorner};
     }
 };
+
 class Wall
 {
 public:
@@ -178,6 +208,7 @@ public:
 void TextRendering_ShowWallsAABBs(GLFWwindow *window, Wall walls[], size_t size);
 
 std::vector<Ball> instanciateLittleBalls();
+std::vector<Cherry> instanciateCherries();
 
 void renderCount(int current_count, char* count_first_digit, char* count_second_digit, char* count_third_digit);
 
@@ -402,6 +433,7 @@ int main(int argc, char *argv[])
 
     // chama a função que inicializa as bolinhas:
     std::vector<Ball> balls = instanciateLittleBalls();
+    std::vector<Cherry> cherries = instanciateCherries();
 
     int initial_ball_count = balls.size();
     int eaten_ball_count = 0;
@@ -592,7 +624,6 @@ int main(int argc, char *argv[])
 
         for (Wall &wall : walls)
         {
-
             wall.render();
             // Teste de colisão com paredes do labirinto
             glm::vec4 collision_direction = checkSphereToAABBCollisionDirection(wall.wall_bbox, pacman_sphere);
@@ -630,6 +661,32 @@ int main(int argc, char *argv[])
         {
             balls.erase(balls.begin() + idx);
             eaten_ball_count += 1;
+        }
+
+        std::vector<int> remove_cherry_indexes = {};
+
+        int cherry_index = 0;
+        for (Cherry &cherry : cherries)
+        {
+            bool ate = checkSphereToSphereCollision(pacman_sphere, cherry.cherry_sphere);
+            if (ate)
+            {
+                remove_cherry_indexes.push_back(cherry_index);
+            }
+            else 
+            {
+                if (isFreeCamOn)
+                {
+                    cherry.modelMatrix = Matrix_Translate(cherry.center.x, cherry.center.y, cherry.center.z) * Matrix_Rotate_X(3.14159f) * Matrix_Rotate_Z(3.14159f) * Matrix_Scale(0.001f, 0.001f, 0.001f);
+                }
+                cherry.render();
+            }
+            cherry_index++;
+        }
+
+        for (int idx : remove_cherry_indexes)
+        {
+            cherries.erase(cherries.begin() + idx);
         }
 
         renderCount(eaten_ball_count, count_first_digit, count_second_digit, count_third_digit);
@@ -801,6 +858,27 @@ std::vector<Ball> instanciateLittleBalls()
 
     return balls;
 };
+
+std::vector<Cherry> instanciateCherries()
+{
+    std::vector<Cherry> cherries;
+    int objectIdCounter = 0;
+
+    float x = -4.5f;
+    float z = -7.0f;
+    for (int i = 0; i < 4; i ++)
+    {
+        Cherry cherry = {Matrix_Translate(x, -0.5f, z)  * Matrix_Rotate_X(3.14159f / 2) * Matrix_Rotate_Z(3.14159f) * Matrix_Scale(0.002f, 0.002f, 0.002f), objectIdCounter++, CHERRY, "Cherry", glm::vec3(x, -0.5f, z), 0.5f};
+        cherries.push_back(cherry);
+
+        if (i % 2 == 0)
+            x = -x;
+
+        z = -z;
+    }
+
+    return cherries;
+}
 
 glm::vec4 cancelCollisionMovement(glm::vec4 movement, std::vector<glm::vec4> collision_directions)
 {
