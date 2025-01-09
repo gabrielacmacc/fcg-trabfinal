8dca4713990dd4e835f9bdb3cdd1dd10ec0522e6
Gabriela Maccagnan <gabrielacmacc@gmail.com>
2025-01-09 19:16:58 -0300

    update name

--- src/main.cpp
+++ src/main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
     // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
     // de pixels, e com título "INF01047 ...".
     GLFWwindow *window;
-    window = glfwCreateWindow(800, 600, "INF01047 - Giulia e Gabriela", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "INF01047 - Trabalho Final - Giulia e Gabriela", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
